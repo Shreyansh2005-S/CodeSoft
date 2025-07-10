902e73da5cd2d13a2212d6689884e94cf099a0f2
Shreyansh Ojha <ojhas1716@gmail.com>
2025-07-10 18:12:16 +0530

    Update Task-2 Calculator.py

--- Task-2 Calculator.py
+++ Task-2 Calculator.py
@@ -1,1 +1,21 @@
+#Simple Calculator using Python
+print("Welcome to the Simple Calculator!")
 
+ch = "yes"
+
+while ch.lower() != "no":
+    try:
+        #Input two numbers
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
+
+        #Menu for operations
+        print("Choose an operation:")
+        print("1. Addition (+)")
+        print("2. Subtraction (-)")
+        print("3. Multiplication (*)")
+        print("4. Division (/)")
+
+        operation = input("Enter your choice (1,2, 3, 4 or +, -, *, /)")
+
+        #Perform the operatio
